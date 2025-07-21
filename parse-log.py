6fbca68e0ffaada632eaf39c7fe429ecb5a17d7e
Badré <145296268+Dobgue@users.noreply.github.com>
2025-07-21 21:26:02 +0100

    Update parse-log.py

--- parse-log.py
+++ parse-log.py
@@ -1,1 +1,15 @@
+# parse_logs.py
 
+failed_logins = {}
+
+with open("sample-logs/auth.log", "r") as file:
+    for line in file:
+        if "Failed password" in line:
+            ip = line.split("from")[1].split()[0]
+            failed_logins[ip] = failed_logins.get(ip, 0) + 1
+
+# Display IPs with more than 3 failed attempts
+print("Suspicious IPs:")
+for ip, count in failed_logins.items():
+    if count > 3:
+        print(f"{ip} => {count} failed login attempts")
